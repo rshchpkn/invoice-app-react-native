f9ad45d19cbe47686cc474e5eb52c59014724957
Anton Rashchepkin <anton.r@2muchcoffee.com>
2019-05-15 19:46:45 +0300

    feat(redux): add redux-devtools-extenstion

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,7 +1,13 @@
-import { createStore } from "redux";
+import { combineReducers, createStore } from "redux";
+import { devToolsEnhancer } from "redux-devtools-extension";
 
-const rootReducer = () => {};
+const rootReducer = combineReducers({
+  invoices: () => {return null},
+  customers: () => {return null},
+  products: () => {return null},
+});
 
 export const store = createStore(
   rootReducer,
+  devToolsEnhancer({}),
 );
